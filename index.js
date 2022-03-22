fb49dafca230ea26b690cb3462f9fa99dfb99639
mad-lina <madelina.ecclesia@gmail.com>
2022-03-22 19:38:09 -0400

    2.5 Part 1: Create endpoint structure

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express'),
     morgan = require('morgan');
-const app = express();
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
+  
+  const app = express();
+  app.use(bodyParser.json());
+
 
 //Morgan used for logging info on server requests
 app.use(morgan('common'));
 
-let topMovies = [
+let movies = [
     {
       title: 'Movie1',
       director: 'Director1'
@@ -48,6 +53,13 @@ let topMovies = [
       }
   ];
 
+let users = [
+  {
+    id = 1,
+    name = test
+  }
+];
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to my movie list!');
@@ -57,8 +69,50 @@ app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
 });
 
+// Gets the list of data about ALL movies
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.json(movies);
+});
+
+// Gets the data about a single movie, by title
+app.get('/movies/:title', (req, res) => {
+  res.json(movies.find((movie) =>
+    { return movies.title === req.params.title }));
+});
+
+// Gets the data about a single genre, by name
+app.get('/movies/genre/:name', (req, res) => {
+  res.send('Successful GET request returning data about a genre')
+});
+
+// Gets the data about a single director, by name
+app.get('/movies/directors/:name', (req, res) => {
+  res.send('Successful GET request returning data about a director')
+});
+
+// Registers a new user
+app.post('/users', (req, res) => {
+  res.status(201).send('Successful POST returning the new user data')
+});
+
+// Allows a user to update their information
+app.put('/users/:username', (req, res) => {
+  res.status(201).send('Successful PUT returning the updated user data')
+});
+
+// Adds a movie to a favorite list
+app.post('/users/:username/movies/:movieID', (req, res) => {
+  res.status(201).send('Successful POST adding movie to favorite list')
+});
+
+// Deletes a movie to a favorite list
+app.delete('/users/:username/movies/:movieID', (req, res) => {
+  res.status(201).send('Successful DELETE movie from favorite list')
+});
+
+// Deregisters a user
+app.delete('/users/:username', (req, res) => {
+  res.status(201).send('Successful DELETE of registered user')
 });
 
 //Printing errors
