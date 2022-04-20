18d683ab018b79da1c3dd7c819b9f198fbb80677
mad-lina <madelina.ecclesia@gmail.com>
2022-04-20 14:25:40 -0400

    Using heroku config variable

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const Users = Models.User;
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 //Connecting to MongoAtlas via Heroku-defined variable CONNECTION_URI
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb+srv://Lina88:Taurus1993!@cluster0.7nexe.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Express Validator for user registration validation
 const { check, validationResult } = require('express-validator');
